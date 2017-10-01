04133d1ee84fd7cc83bef6148b4e7f12368d547c
Andreas Caravella <andreas.caravella@gmail.com>
2017-10-02 00:43:45 +0200

    add build revision

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #exit if any command fails
 set -e
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
 dotnet pack src/Sendowl -c Release -o ../../nuget --version-suffix=$revision 

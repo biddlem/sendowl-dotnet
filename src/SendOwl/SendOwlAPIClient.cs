1e0fe6b9b7e13b1f30f4daa672129dd383d01734
Andreas Caravella <andreas.caravella@gmail.com>
2017-10-02 00:07:31 +0200

    lowercase url

--- src/SendOwl/SendOwlAPIClient.cs
+++ src/SendOwl/SendOwlAPIClient.cs
@@ -5,7 +5,7 @@ namespace SendOwl
 {
     public class SendOwlAPIClient : IDisposable
     {
-        private const string BaseUrl = "https://www.SendOwl.com/api/v1/";
+        private const string BaseUrl = "https://www.sendowl.com/api/v1/";
         internal readonly IHttpSerializerClient client;
         public ProductEndpoint Product => new ProductEndpoint(client);
         public BundleEndpoint Bundle => new BundleEndpoint(client);

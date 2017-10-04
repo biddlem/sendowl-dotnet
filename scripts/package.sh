ca8dd19341322e924f8e57e79d2e4f2ffeaae828
Andreas Caravella <andreas.caravella@gmail.com>
2017-10-04 13:39:17 +0200

    Fix bad merge

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,4 +3,4 @@
 set -e
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
-dotnet pack src/Sendowl -c Release -o ../../nuget --version-suffix=$revision dotnet pack src/SendOwl -c Release -o ../../nuget --version-suffix=ci-$revision 
+dotnet pack src/SendOwl -c Release -o ../../nuget --version-suffix=ci-$revision 

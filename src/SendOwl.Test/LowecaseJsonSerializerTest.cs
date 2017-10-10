12e955a4e41fff80cb30eb0c09b4922ca208c639
naile <andreas.caravella@gmail.com>
2017-10-10 20:42:57 +0200

    Add test for json serialization

---
+++ src/SendOwl.Test/LowecaseJsonSerializerTest.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace SendOwl.Test
+{
+    public class LowecaseJsonSerializerTest
+    {
+        public enum TestEnum
+        {
+            SomeValue = 0
+        }
+
+        [Fact]
+        public void SerializeObject_Serializes_properties_and_enums_to_lowercase()
+        {
+            var someObject = new
+            {
+                MyEnum = TestEnum.SomeValue
+            };
+            var json = LowercaseJsonSerializer.SerializeObject(someObject);
+
+            json.ShouldBe("{\"myEnum\":\"someValue\"}");
+        }
+    }
+}

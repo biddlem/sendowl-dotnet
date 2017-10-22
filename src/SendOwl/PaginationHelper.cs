86daab73297e0eceb0d989e574e8265b601451d6
Andreas Caravella <andreas.caravella@gmail.com>
2017-10-22 19:28:27 +0200

    Meaningful naming of type parameters

--- src/SendOwl/PaginationHelper.cs
+++ src/SendOwl/PaginationHelper.cs
@@ -8,15 +8,15 @@ namespace SendOwl
     public class PaginationHelper
     {
         private const int ItemsPerPage = 50;
-        public static async Task<List<TResult>> GetAllAsync<TResult, Y>(IHttpSerializerClient client, string relativeUrl, Func<Y, TResult> selector)
+        public static async Task<List<TObject>> GetAllAsync<TObject, TCollection>(IHttpSerializerClient client, string relativeUrl, Func<TCollection, TObject> selector)
         {
-            var items = new List<TResult>();
+            var items = new List<TObject>();
             var delimiter = relativeUrl.Contains("?") ? "&" : "?";
             var page = 1;
             while (true)
             {
                 var query = $"{delimiter}per_page={ItemsPerPage}&page={page}";
-                var response = await client.GetAsync<IEnumerable<Y>>(relativeUrl + query).ConfigureAwait(false);
+                var response = await client.GetAsync<IEnumerable<TCollection>>(relativeUrl + query).ConfigureAwait(false);
                 if (!response.Any()) break;
                 items.AddRange(response.Select(selector));
                 page++;
